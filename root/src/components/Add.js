8fa63cdb50e4a816026f577e74322410eb68d15b
Tofiq Huseynli <tofiq789@gmail.com>
2023-08-01 17:26:03 +0400

    master

--- root/src/components/Add.js
+++ root/src/components/Add.js
@@ -3,39 +3,43 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import "../style/post.css";
 import Postmet from "./Postmet";
-function Add() {
+function Add({ data }) {
   const [show, setShow] = React.useState(false);
-  const [name,setName]= React.useState("");
-  const [username,setUserName]= React.useState("");
+  const [name, setName] = React.useState("");
+  const [username, setUserName] = React.useState("");
   const handleShow = () => {
     setShow(true);
   };
   const handleClose = () => {
     setShow(false);
   };
-  const handleAdd = ()=>{
-    fetch("http://localhost:8000/blogs",{
-        method: 'POST',
+
+  const add = () => {
+    if (name == "" || username == "") {
+      window.alert("Empty");
+    } else {
+      fetch("http://localhost:8000/blogs", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           name: name,
-          username:username
-
-        })
-       
-    })
-}
-
+          username: username,
+        }),
+      });
+      data();
+      setName("");
+      setUserName("");
+    }
+    
+  };
   return (
     <div>
       <>
-      {name}
-      <br></br>
-      {username}
-      <Postmet name={name} username={username} />
-      <div className="d-flex justify-content-end pt-2 px-3 ">
-        <Button  variant="primary" onClick={handleShow}>
-          Add
-        </Button>
+        <Postmet name={name} username={username} />
+        <div className="d-flex justify-content-end pt-2 px-3">
+          <Button variant="primary" onClick={handleShow}>
+            Add
+          </Button>
         </div>
 
         <Modal show={show} onHide={handleClose}>
@@ -43,25 +47,39 @@ function Add() {
             <Modal.Title>Add</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <div className="mb-3">  <label >Name</label>
-            <input onChange={(e)=>setName(e.target.value)} className="form-control"/>
+            <div className="mb-3">
+              {" "}
+              <label>Name</label>
+              <input
+                onChange={(e) => setName(e.target.value)}
+                className="form-control"
+              />
             </div>
-            <div className="mb-3">  <label >Username</label>
-            <input onChange={(e)=>setUserName(e.target.value)} className="form-control"/>
+            <div className="mb-3">
+              {" "}
+              <label>Username</label>
+              <input
+                onChange={(e) => setUserName(e.target.value)}
+                className="form-control"
+              />
             </div>
-            <div className="mb-3">  <label >Email</label>
-            <input className="form-control"/>
+            <div className="mb-3">
+              {" "}
+              <label>Email</label>
+              <input className="form-control" />
             </div>
-          
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               Close
             </Button>
-            <Button  variant="primary" onClick={()=>{
-                handleAdd();
-                handleClose()
-            }}>
+            <Button
+              variant="primary"
+              onClick={() => {
+                add();
+                handleClose();
+              }}
+            >
               ADD
             </Button>
           </Modal.Footer>
--- root/src/pages/Employers.js
+++ root/src/pages/Employers.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../style.css";
 import Add from "../components/Add";
+import Delete from "../components/Delete";
 
 
 function Employers() {
@@ -19,9 +20,21 @@ function Employers() {
   React.useEffect(() => {
     data();
   }, []);
+
+  
+    async function handleDelete(id){
+      await fetch(`http://localhost:8000/blogs/${id}`,
+      {method: 'DELETE'});
+      data()
+    }
+ 
+
+
+
   return (
     <>
-      <Add/>
+      <Add data={data}/>
+      <Delete/>
       <table>
         <tr className="head">
           <th>Name</th>
@@ -30,10 +43,11 @@ function Employers() {
         </tr>
 
         {employer.map((item, key) => (
-          <tr className="elements" key={key}>
-            <td>{item.name}</td>
+          <tr className="elements" key={item.id}>
+            <td>{item.name} </td>
             <td>{item.username}</td>
             <td>{item.email}</td>
+            <td className="btn-data"><button className="btn btn-danger" onClick={()=>handleDelete(item.id)}>Del</button></td>
           </tr>
         ))}
       </table>
